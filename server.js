791a2270b82312ca53a2e6bf01eb7de28e893e31
abraham-yusuf <1703020028@students.unis.ac.id>
2021-04-21 06:35:11 +0700

    add dynamic GET route

--- server.js
+++ server.js
@@ -30,5 +30,13 @@ app.listen(8000,function(){
    })
   })
   
+  app.get('/users/:id',function(req,res){
+   console.log(req.params.id)
+   res.json({
+    success: true,
+    message: 'got one user',
+    user: req.params.id
+   })
+  })
   app.listen(8000,function(){console.log('server is listening')})
-  
+
